d0342ab138deddb7f627fa4e46536341dda9a2ef
hema <hema.v181@gmail.com>
2023-03-28 19:40:47 +0530

    changes on tax invoice

--- ziva_Project/settings.py
+++ ziva_Project/settings.py
@@ -119,12 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
 
 STATIC_DIRS = [
     os.path.join(BASE_DIR,'ziva_app/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
